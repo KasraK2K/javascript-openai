e4418db6e5843eabfc544e22a7852444e59f44ff
Kasra Karami <Kasra.Karami.KK@gmail.com>
2025-02-27 16:52:55 +0330

    add prettier

---
+++ src/chat.js
@@ -0,0 +1,69 @@
+/* ------------------------------ Node Modules ------------------------------ */
+import readline from 'node:readline'
+/* ------------------------------ Dependencies ------------------------------ */
+import chalk from 'chalk'
+/* ----------------------------- Custom Modules ----------------------------- */
+import { openai } from './utils/openai.js'
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------- Constants ------------------------------- */
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+/* -------------------------------------------------------------------------- */
+
+/**
+ * @param {{role: 'assistant' | 'function' | 'system' | 'tool' | 'user'; content: string}} history
+ * @param {string} message
+ * @returns {{role: "assistant"; content: string}}
+ */
+async function newMessage(history, message) {
+    const response = await openai.chat.completions.create({
+        model: 'gpt-4o-mini',
+        messages: [...history, message],
+        temperature: 2
+    })
+
+    return response.choices[0].message
+}
+
+/**
+ * @param {string} userInput
+ * @returns {{role: "user"; content: string}}
+ */
+function formatMessage(userInput) {
+    return { role: 'user', content: userInput }
+}
+
+function chat() {
+    const history = [
+        {
+            role: 'system',
+            content: 'You are an AI assistant.'
+        }
+    ]
+
+    function start() {
+        rl.question('You: ', async (userInput) => {
+            if (userInput.toLowerCase() === 'exit') {
+                rl.close()
+                return
+            }
+
+            const userMessage = formatMessage(userInput)
+            const response = await newMessage(history, userMessage)
+            history.push(userMessage, response)
+            console.log(`\n\nAI: ${response.content}`)
+
+            start()
+        })
+    }
+
+    console.log('AI: how can I help you today?')
+    start()
+}
+
+console.log("Chatbot initialized. type 'exit' to end the chat")
+
+chat()
--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,9 +1,9 @@
 /* ------------------------------ Dependencies ------------------------------ */
-import OpenAI from "openai";
-import dotenv from "dotenv";
-dotenv.config();
+import OpenAI from 'openai'
+import dotenv from 'dotenv'
+dotenv.config()
 /* -------------------------------------------------------------------------- */
 
 export const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY,
-});
+    apiKey: process.env.OPENAI_API_KEY
+})
